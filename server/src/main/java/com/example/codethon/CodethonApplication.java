2fd931a1206182e51a5a096e6fef3f810a21bd27
Carlos Oliveira <carlos.junior@elotech.com.br>
2024-06-28 00:02:14 -0300

    iniciando projeto do backend

---
+++ server/src/main/java/com/example/codethon/CodethonApplication.java
@@ -0,0 +1,13 @@
+package com.example.codethon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodethonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodethonApplication.class, args);
+	}
+
+}
